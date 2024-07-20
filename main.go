71815faa24336c3273444dd4316b0a108bbb12f3
harsh28 <harsh.srivastava@signzy.com>
2024-07-20 18:39:33 +0530

    added env setup

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+    "log"
+    "os"
+    "github.com/joho/godotenv"
     "github.com/gin-gonic/gin"
     "github.com/harshsri28/apica/module"
     "github.com/harshsri28/apica/routes"
 )
 
 func main() {
-    port := "3000"
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        log.Fatal("PORT environment variable not set")
+    }
 
     lruCache := cache.NewLRUCache(10)
     router := gin.New()
