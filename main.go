94bda80f09d4d88b88120e8e8318b199d897e14e
harsh28 <harsh.srivastava@signzy.com>
2024-07-31 01:47:10 +0530

    added cor domain url

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 
     // Configured CORS
     router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowOrigins:     []string{"http://localhost:3000","https://lrucache-backend.onrender.com"},
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type"},
         ExposeHeaders:    []string{"Content-Length"},
